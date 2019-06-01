6c591ed21c5a19643dc1859ad52aa82129addbc7
mikhail.eshchenko <mikhail.eshchenko@gmail.com>
2019-06-01 13:05:58 +0300

    build a broject

---
+++ build/precache-manifest.4364df84574fe35b669110084ef522ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcfa85b8a7e01d51f230c04c3aa7220",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf5d9289efae1ab8aad3",
+    "url": "/static/css/main.eeb9e412.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "bf5d9289efae1ab8aad3",
+    "url": "/static/js/main.d38b9a46.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
